f81bd52983c49b52801c8b1a47999a989029ee4f
NSMoreira145 <nicolassm145@gmail.com>
2023-11-30 01:12:14 -0300

    Mudanças no Card e SearchPage

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from "react";
 import "../styles/components/card.css";
 
-
-
-const Card = ( ) => {
-    const [winner, setWinner] = useState(null);
-    const [team, setTeam] = useState(null);
-    const [fastestLap, setFastestLap] = useState(null);
-    const [place, setPlace] = useState(null);    
-
-    useEffect(() => {
-        const corrida = 'https://ergast.com/api/f1/current/last/results.json?season=2023';
-        
-        fetch(corrida)
-        .then(response => response.json())
-        .then(data => {
-            const race = data.MRData.RaceTable.Races[0];
-            setWinner(race.Results[0].Driver);
-            setTeam(race.Results[0].Constructor); 
-            setFastestLap(race.Results[0].FastestLap.Time);
-            setPlace(race.raceName);
-        });
-    },  
-    []);
-
-    return (<>
-       <div className="card">
-            <div className="img"></div>
-            <div className="text">
-                <ul className="card-list">
-                    <li><p className="card-text">VENCEDOR: <span className ="card-span">{winner ? `${winner.givenName} ${winner.familyName}` : " "}</span> </p></li>
-                    <li><p className="card-text">EQUIPE: <span className ="card-span">{team ? `${team.name}` : " "}</span></p></li>
-                    <li><p className="card-text">VOLTA MAIS RÁPIDA: <span className ="card-span">{fastestLap ? `${fastestLap.time}` : " "}</span></p></li>
-                    <li><p className="card-text">LOCAL DA PROVA: <span className ="card-span">{place ? place : " "}</span></p></li>
-                </ul>
-            </div>
-            <div className="rating">    
-                <p>★★★★★</p>
-            </div>
+const Card = ({ winner, team, fastestLap, place }) => {
+  return (
+    <>
+      <div className="card">
+        <div className="img"></div>
+        <div className="text">
+          <ul className="card-list">
+            <li>
+              <p className="card-text">
+                VENCEDOR: <span className="card-span">{winner}</span>{" "}
+              </p>
+            </li>
+            <li>
+              <p className="card-text">
+                EQUIPE:{" "}
+                <span className="card-span">{team}</span>
+              </p>
+            </li>
+            <li>
+              <p className="card-text">
+                VOLTA MAIS RÁPIDA:{" "}
+                <span className="card-span">
+                  {fastestLap}
+                </span>
+              </p>
+            </li>
+            <li>
+              <p className="card-text">
+                LOCAL DA PROVA:{" "}
+                <span className="card-span">{place}</span>
+              </p>
+            </li>
+          </ul>
         </div>
-    </>); 
+        <div className="rating">
+          <p>★★★★★</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Card;
--- front/src/pages/auth/SearchPage.jsx
+++ front/src/pages/auth/SearchPage.jsx
@@ -1,45 +1,91 @@
 import "../../styles/auth/searchpage.css";
 import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import RaceDetails from "./components/RaceDetails";
-import Card from "../../components/card";
+import Card from "../../components/Card";
 
 const SearchPage = () => {
+  const [winners, setWinners] = useState([]);
+  const [teams, setTeams] = useState([]);
+  const [fastestLaps, setFastestLaps] = useState([]);
+  const [places, setPlaces] = useState([]);
 
-    const i = 5;
+  const fetchData = async () => {
+    const corrida = "https://ergast.com/api/f1/2023/results.json?limit=1000";
 
-    return (
-        <>
-        <Header/>
-        <div className="main-body">
-            <div className="main-search-container">
-                <div className="main-search">
-                    <SearchBar
-                        label={""} 
-                        placeholder={"Pesquise piloto, pista ou equipe!"} 
-                        type={"text"} 
-                        name={"search"}
-                        classNm={"search-bar"}
-                        />
-                </div>
-                {Array(i).fill().map((_, index) => (
-                        <Card key={index} />
-                    ))}
+    const response = await fetch(corrida);
+    const data = await response.json();
+    const winnerArray = [];
+    const teamArray = [];
+    const fastestLapArray = [];
+    const placeArray = [];
+
+    data.MRData.RaceTable.Races.forEach((race) => {
+      winnerArray.push(race.Results[0].Driver.driverId);
+      teamArray.push(race.Results[0].Constructor.name);
+      fastestLapArray.push(race.Results[0].FastestLap.Time.time);
+      placeArray.push(race.raceName);
+    });
+
+    setWinners(winnerArray);
+    setTeams(teamArray);
+    setFastestLaps(fastestLapArray);
+    setPlaces(placeArray);
+  };
+
+  const dadosCorrida = winners.map((winner, index) => ({
+    winners: winner,
+    teams: teams[index],
+    fastestLaps: fastestLaps[index],
+    places: places[index],
+  }));
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="main-body">
+        <div className="main-search-container">
+          <div className="main-search">
+            <SearchBar
+              label={""}
+              placeholder={"Pesquise piloto, pista ou equipe!"}
+              type={"text"}
+              name={"search"}
+              classNm={"search-bar"}
+            />
+          </div>
+
+          {dadosCorrida.map((dados, index) => (
+            <Card
+              winner={dados.winners}
+              team={dados.teams}
+              fastestLap={dados.fastestLaps}
+              place={dados.places}
+              key={index}
+            />
+          ))}
         </div>
         <div className="main-content-container">
-                <div className="content">
-                    <div className="content-main">
-                        <> 
-                            <RaceDetails corrida={"https://ergast.com/api/f1/current/last/results.json?limit=1"}/>
-                        </>
-                    </div>
-                </div>
+          <div className="content">
+            <div className="content-main">
+              <>
+                <RaceDetails
+                  corrida={
+                    "https://ergast.com/api/f1/current/last/results.json?limit=1"
+                  }
+                />
+              </>
             </div>
-        </div>  
-        
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
