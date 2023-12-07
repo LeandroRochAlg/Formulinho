18b136a07034564ec9c7d4f107d07043cd185f25
flp-gregorio <felipealvesgregorio01@gmail.com>
2023-12-07 04:18:52 -0300

    Add routes for public access

--- front/src/routers/PrivateRoutes.jsx
+++ front/src/routers/PrivateRoutes.jsx
@@ -3,18 +3,21 @@ import ProfilePage from "../pages/system/ProfilePage.jsx";
 import SearchPage from "../pages/system/SearchPage.jsx";
 import HomePage from "../pages/system/HomePage.jsx";
 import NotFoundPage from "../pages/errors/Page404.jsx";
-
+import PrivateRouter from "./PrivateRouter.jsx";
 import { Routes, Route } from "react-router-dom";
 
 const PrivateRoutes = () => {
   return (
-    <Routes>
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+        <>
+          <PrivateRouter path="/about" element={<AboutPage />} />
+        </>
+      <Routes>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+    </Routes></>
   );
 };
 
